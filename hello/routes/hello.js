6842fa1c23c06f405a6283db33e92765ccf2f0e1
Anya Bouzida <anyalouise@gmail.com>
2021-01-20 22:47:09 -0800

    finalize node.js stuff

--- hello/routes/hello.js
+++ hello/routes/hello.js
@@ -3,8 +3,14 @@
  * GET home page.
  */
 
+
 exports.view = function(req, res){
+  var nameToShow = req.params.userName;
+  if (nameToShow == undefined){
+  	nameToShow = 'You';
+  }
+  console.log("name is " + nameToShow);
   res.render('index', {
-  	'name': '',
+  	'name': nameToShow,
   });
 };
